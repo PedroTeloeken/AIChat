dd2d373e6acad1ec7fa2c5b925e2f1796e7e0dba
pedroteloeken <pedroteloeken2211@gmail.com>
2025-11-12 08:21:45 -0300

    Initial Commit

---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Pedro Teloeken on 12/11/25.
+//
+
+import Testing
+@testable import AIChat
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
