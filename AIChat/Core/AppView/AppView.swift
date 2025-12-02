7ca9500c57db1a83cfef90a38fe1744fd0bf9b5c
pedroteloeken <pedroteloeken2211@gmail.com>
2025-12-02 07:05:47 -0300

    [Feature] Add AppView, AppViewBuilder

---
+++ AIChat/Core/AppView/AppView.swift
@@ -0,0 +1,39 @@
+//
+//  AppView.swift
+//  AIChat
+//
+//  Created by Pedro Teloeken on 13/11/25.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    
+    @State var showTabBar: Bool = false
+        
+    var body: some View {
+        AppViewBuilder(
+            showTabBar: showTabBar,
+            tabbarView: {
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    Text("Tabbar")
+                }
+            },
+            onboardingView: {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Onboarding")
+                }
+            }
+        )
+    }
+}
+
+#Preview("AppView - Tabbar") {
+    AppView(showTabBar: true)
+}
+
+#Preview("AppView - Onboarding") {
+    AppView(showTabBar: false)
+}
---
+++ AIChat/Core/AppView/AppViewBuilder.swift
@@ -0,0 +1,60 @@
+//
+//  AppViewBuilder.swift
+//  AIChat
+//
+//  Created by Pedro Teloeken on 02/12/25.
+//
+
+import SwiftUI
+
+struct AppViewBuilder<TabbarView: View, OnboardingView: View>: View {
+    @State var showTabBar: Bool = false
+    
+    @ViewBuilder var tabbarView: TabbarView
+    @ViewBuilder var onboardingView: OnboardingView
+    
+    var body: some View {
+        ZStack {
+            if showTabBar {
+                tabbarView
+                    .transition(.move(edge: .trailing))
+            } else {
+                onboardingView
+                    .transition(.move(edge: .leading))
+            }
+        }
+        .animation(.smooth, value: showTabBar)
+    }
+}
+
+private struct PreviewView: View {
+    
+    @State private var showTabBar: Bool = false
+    
+    @State private var showOnboardingView: Bool = false
+    
+    var body: some View {
+        AppViewBuilder(
+            showTabBar: showTabBar,
+            tabbarView: {
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    Text("Tabbar")
+                }
+            },
+            onboardingView: {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Onboarding")
+                }
+            }
+        )
+        .onTapGesture {
+            showTabBar.toggle()
+        }
+    }
+}
+
+#Preview {
+    PreviewView()
+}
