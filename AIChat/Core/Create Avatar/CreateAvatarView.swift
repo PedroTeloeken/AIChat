980c834efd7c60093fdc839fd152737e9b2a95a3
pedroteloeken <pedroteloeken2211@gmail.com>
2025-12-30 17:58:39 -0300

    [Feature] Create Avatar View

---
+++ AIChat/Core/Create Avatar/CreateAvatarView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAvatarView.swift
+//  AIChat
+//
+//  Created by Pedro Teloeken on 29/12/25.
+//
+
+import SwiftUI
+
+struct CreateAvatarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAvatarView()
+}
